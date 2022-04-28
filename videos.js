1987fef1720521524d4874c0751c9df727a9cacf
paola <astridpaolafuentesurbina@gmail.com>
2022-04-28 13:22:10 +0200

    mau-y-ricky

---
+++ videos.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="keywords" content="description">
+	<meta name="description" content="description">
+	<link href="style.css" rel="stylesheet">
+	<link href="style.css" rel="stylesheet">
+	<meta name="author" content="Paola">
+	<link rel="icon" type="icon.png" href="favicon.png">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title>Mau y ricky</title>
+	<script type="videos.js"></script>
+</head>
+<body>
+
+</body>
+</html>
